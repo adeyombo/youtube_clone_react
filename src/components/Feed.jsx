3446cce5258c6c68ceb9c70102a6fc5162c96ac4
Adebola <adeyomboadebola@gmail.com>
2022-08-28 19:01:56 +0100

    branding edit

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -34,12 +34,7 @@ const Feed = () => {
         </Typography>
       </Box>
 
-      <Box sx={{
-        p: 2,
-        overflowY: 'auto',
-        height: '90vh',
-        flex: 2
-      }}>
+      <Box sx={{p: 2, overflowY: 'auto', height: '90vh', flex: 2}}>
         <Typography variant='h4' fontWeight='bold' mb={2} sx={{color: 'white'}}>
           {selectedCategory} <span style={{ color: '#F31503' }}>videos</span>
         </Typography>
